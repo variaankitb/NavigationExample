94400493d6de2a33edecd0f6d267ddbe3f3595c3
Ankit Varia <variaankit@hotmail.com>
2020-09-15 17:59:57 +0530

    Initial commit

---
+++ app/src/main/java/com/avtechnology/navigationexample/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.avtechnology.navigationexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_first.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //Click to navigate from first fragment to second fragment
+        navigation_button?.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/avtechnology/navigationexample/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.avtechnology.navigationexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_second.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        second_navigation?.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+    }
+}
